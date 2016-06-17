f2dc6f9a7a58f8b8c5a960eb1ea0795303952827
Alan Chen <alanc1995@gmail.com>
2016-06-16 22:19:21 -0400

    Cleaned up files

--- src/TestAI.java
+++ src/TestAI.java
@@ -1,6 +1,9 @@
 /**
  * Created by Alan on 4/23/16.
  */
+
+/* Run this file to create Earth and start Earth */
+
 public class TestAI {
     public static void main(String[] args) {
         Environment.getInstance().startEarth(100);
